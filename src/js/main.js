fb488d1e145d01815d0bc7b209198cdf80fece05
Devin Liang <devinliang@gmail.com>
2025-12-21 23:54:58 +0800

    aos OK

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,9 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 // 2. 初始化 AOS
-document.addEventListener('DOMContentLoaded', () => {
-    AOS.init({
-        duration: 1000, // 動畫持續時間 (毫秒)
-        easing: 'ease-in-out', // 動畫曲線
-        once: true, // true = 動畫只執行一次 (向下滑經過時觸發，向上滑不重複)
-        offset: 100, // 觸發動畫的偏移量 (距離底部多少 px 時觸發)
-    });
+AOS.init({
+  duration: 800,
+  easing: 'ease-in-out',
+  once: true
 });
+
